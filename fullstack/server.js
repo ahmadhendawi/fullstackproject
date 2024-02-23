825015390263b2538aff51fa62bccd2d1dd61d99
ahmadhendawi <ahmad.hendawi200255@gmail.com>
2024-02-24 00:31:08 +0300

    change

--- fullstack/server.js
+++ fullstack/server.js
@@ -3,7 +3,7 @@ const cors= require("cors")
 const app=express()
 app.use(cors());
 
-var server=app.listen(3000, function(){
+var server=app.listen(4000, function(){
 var host=server.address().address
 var port=server.address().port
 })
@@ -18,5 +18,5 @@ app.get('/',function(req,res){
     
 
     app.get('/getPrice',function(req,res){
-            res.json({price1:"20jds", price2:"40jds"})
+            res.json({price1:"20jds", price2:"40jds", price3:"33jds", price4:"56jds"})
     })
--- fullstack/src/App.js
+++ fullstack/src/App.js
@@ -1,30 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 var result=res.json
-
-function getData(){
-  fetch('https://localhost:4000/getData')
+class App extends React.Component
+{
+  constructor (props){
+    super(props) 
+    this.state={
+      data:"",
+      price:""}
+  }
+data(){
+  fetch('http://localhost:4000/getData')
   .then(response => response.json())
-  .then(json => result=json)
+  .then(json => this.setState({data:json}))
   .catch(error => console.error(error));
 }
-
-function getPrice(){
-  fetch('https://localhost:4000/getPrice')
+products(){
+  fetch('http://localhost:4000/getPrice')
   .then(response => response.json())
-  .then(json => result=json)
+  .then(json => this.setState({price:json}))
   .catch(error => console.error(error));
 }
-
-function App() {
-  {getData()} {getPrice()}
-  return (
-
-    <p>{result.name.age.major}</p>
-  
-  );
+fetchData=()=>
+{
+   this.data();
+   this.products();
 }
+render()
+{
+   return (
+    <div>
+      <button onClick={this.fetchData}>fetch </button>
+      <p>{this.state.data.name}</p>
+      <p>{this.state.data.age}</p>
+      <p>{this.state.data.major}</p>
 
+      <div><p>{this.state.price.price1}</p>
+      <p>{this.state.price.price2}</p>
+      <p>{this.state.price.price3}</p>
+    <p>{this.state.price.price4}</p></div>
+    </div>
+   )
+}}
 export default App;
 
